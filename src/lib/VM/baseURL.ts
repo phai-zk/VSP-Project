54022daac25d80bf0f5d5c0f67b738f35c2f55e3
phai-zk <chisanupong.m34.1@gmail.com>
2025-10-13 03:12:31 +0700

    URL

--- src/lib/VM/baseURL.ts
+++ src/lib/VM/baseURL.ts
@@ -1,1 +1,2 @@
-export let URL: string = "https://vsp-project.vercel.app/api";
+// export let URL: string = "https://vsp-project.vercel.app/api";
+export let URL: string = "http://pub.dmaltzi.com:8000/";
